c4d9565564e6e983c83c9ddd4bf7da9af5c1ff5c
SeolYang <kyowonhorang@gmail.com>
2021-04-25 17:35:39 +0900

    다수의 Entity와 Light를 가지고, 관리하는 Scene 클래스를 추가하였습니다.

---
+++ Sources/Rendering/Scene.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <vector>
+#include <string>
+
+class Entity;
+class Light;
+class Scene
+{
+public:
+   Scene() = default;
+   virtual ~Scene();
+
+   Entity& CreateEntity(const std::string_view name);
+   Light& CreateLight();
+
+   [[nodiscard]]
+   const std::vector<Entity*>& GetEntities() const { return m_entities; }
+   [[nodiscard]]
+   const std::vector<Light*>& GetLights() const { return m_lights; }
+
+private:
+   std::vector<Entity*> m_entities;
+   std::vector<Light*> m_lights;
+
+};
