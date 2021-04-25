c16b4749a159623fb08bb4689f12f039a5a14863
SeolYang <kyowonhorang@gmail.com>
2021-04-25 17:19:33 +0900

    Scene 상에 놓이는 물체를 표현하기 위해 Entity 클래스를 추가하였습니다

---
+++ Sources/Rendering/Entity.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include <glm/glm.hpp>
+
+class Model;
+class Shader;
+class Entity
+{
+public:
+   Entity() :
+   m_transform(glm::mat4()),
+   m_model(nullptr)
+   {
+   }
+
+   Entity(const Entity& rhs) = default;
+   Entity(Entity&& rhs) noexcept :
+      m_transform(rhs.m_transform),
+      m_model(rhs.m_model)
+   {
+      rhs.m_model = nullptr;
+      rhs.m_transform = glm::mat4();
+   }
+
+   Entity& operator=(const Entity& rhs) = default;
+   Entity& operator=(Entity&& rhs) noexcept
+   {
+      m_transform = rhs.m_transform;
+      m_model = rhs.m_model;
+      rhs.m_model = nullptr;
+      rhs.m_transform = glm::mat4();
+
+      return (*this);
+   }
+
+   ~Entity() = default;
+
+   void SetModel(Model* model) { m_model = model; }
+
+   [[nodiscard]]
+   Model* GetModel() const { return m_model; }
+
+   void SetTransform(const glm::mat4& transform) { m_transform = transform; }
+
+   [[nodiscard]]
+   glm::mat4 GetTransform() const { return m_transform; }
+
+   void Render(Shader& shader) const;
+
+private:
+   glm::mat4 m_transform;
+   Model* m_model;
+
+};
